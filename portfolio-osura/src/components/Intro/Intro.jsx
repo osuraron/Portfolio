5fa9ef3879adb3ba7a91567a0d793c81109e21d8
osura <osura.ronath@gmail.com>
2023-04-03 12:59:23 +0400

    intro page done

--- portfolio-osura/src/components/Intro/Intro.jsx
+++ portfolio-osura/src/components/Intro/Intro.jsx
@@ -9,7 +9,17 @@ export default function Intro() {
         <img src="src/assets/man.png" alt="" />
       </div>
     </div>
-    <div className="right"></div>
+
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm </h2>
+        <h1>Osura Ronath</h1>
+        <h3>Software Engineer <span> Photographer </span> </h3>
+      </div>
+      <a href="#portfolio">
+        <img src="src/assets/down.png" alt="" />
+      </a>
+    </div>
   </div>
   );
 }
